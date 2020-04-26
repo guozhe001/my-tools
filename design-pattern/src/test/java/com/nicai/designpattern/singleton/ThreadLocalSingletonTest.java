e1cceba1bf19c5235a0ddc35df35f3eb2ed9a7ac
guozhe <guozhe@renmaitech.com>
2020-04-26 14:06:05 +0800

    代码优化

--- design-pattern/src/test/java/com/nicai/designpattern/singleton/ThreadLocalSingletonTest.java
+++ design-pattern/src/test/java/com/nicai/designpattern/singleton/ThreadLocalSingletonTest.java
@@ -1,20 +1,19 @@
 package com.nicai.designpattern.singleton;
 
+import com.google.common.base.Stopwatch;
 import com.google.common.collect.Lists;
-import lombok.AllArgsConstructor;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
@@ -24,55 +23,70 @@ import java.util.stream.IntStream;
 @Slf4j
 public class ThreadLocalSingletonTest {
 
+    /**
+     * 线程池的最大线程数量
+     */
+    private static final int MAXIMUM_POOL_SIZE = 10;
+    private static final String THREAD_NAME_FORMAT = "test-ThreadLocalSingleton-%s";
+
     @Rule
     public SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 
-    private static final ThreadFactory threadFactory = new ThreadFactory() {
-        private AtomicInteger i = new AtomicInteger();
+    private static final ThreadFactory threadFactoryByGuava = new ThreadFactoryBuilder().setNameFormat(THREAD_NAME_FORMAT).build();
 
-        @Override
-        public Thread newThread(Runnable r) {
-            return new Thread(r, "test-ThreadLocalSingleton-" + i.incrementAndGet());
-        }
-    };
-
-    private static final ExecutorService executorService = new ThreadPoolExecutor(10, 10, 0L, TimeUnit.MILLISECONDS,
+    private static final ExecutorService executorService = new ThreadPoolExecutor(MAXIMUM_POOL_SIZE, MAXIMUM_POOL_SIZE, 0L, TimeUnit.MILLISECONDS,
             new LinkedBlockingDeque<Runnable>() {
-            }, threadFactory);
+            }, threadFactoryByGuava);
 
     @Test
-    public void getInstance() throws InterruptedException {
+    public void getInstance() {
+        Stopwatch stopwatch = Stopwatch.createStarted();
         List<ThreadInstance> threadInstances = Lists.newArrayList();
-        IntStream.range(0, 1000).forEach(i -> executorService.execute(() -> {
-            ThreadLocalSingleton instance = ThreadLocalSingleton.getInstance();
-            log.info("instance={}", instance);
-            threadInstances.add(new ThreadInstance(Thread.currentThread().getName(), instance));
-        }));
-//        String logs = systemOutRule.getLog();
-//        String[] split = StringUtils.split(logs, '\n');
-//        for (String logLine : split) {
-//            System.out.println(logLine);
-//        }
-        Thread.sleep(1000L);
-        Map<String, List<ThreadInstance>> collect = threadInstances.stream().collect(Collectors.groupingBy(ThreadInstance::getThreadName));
-        collect.forEach((k, v) -> {
-            log.info("k={},value size={}", k, v.size());
-            check(v);
-        });
+        // 使用线程池执行1000次获取instance
+        IntStream.range(0, 1000).forEach(i -> executorService.execute(() -> threadInstances.add(new ThreadInstance(Thread.currentThread().getName(), ThreadLocalSingleton.getInstance()))));
+        waitAllThreadDone(stopwatch);
+//        Map<String, List<ThreadInstance>> collect = threadInstances.stream().collect(Collectors.groupingBy(ThreadInstance::getThreadName));
+//        collect.forEach((threadName, threadInstanceList) -> {
+//            log.info("threadName={} size={}", threadName, threadInstanceList.size());
+//            check(threadInstanceList);
+//        });
     }
 
-    private void check(List<ThreadInstance> v) {
-        Iterator<ThreadInstance> iterator = v.iterator();
-        ThreadInstance next = iterator.next();
+
+    private void waitAllThreadDone(Stopwatch stopwatch) {
+        executorService.shutdown();
+        // 等待所有的线程执行结束
+        while (true) {
+            if (executorService.isTerminated()) {
+                log.info("all task is done spend {} MILLISECONDS", stopwatch.elapsed(TimeUnit.MILLISECONDS));
+                break;
+            }
+        }
+    }
+
+    /**
+     * 校验列表中的实例是相同的
+     *
+     * @param threadInstances 实例列表
+     */
+    private void check(List<ThreadInstance> threadInstances) {
+        Iterator<ThreadInstance> iterator = threadInstances.iterator();
+        ThreadInstance first = iterator.next();
         if (iterator.hasNext()) {
-            next = iterator.next();
+            ThreadInstance next = iterator.next();
+            Assert.assertEquals(first, next);
         }
     }
 
     @Data
     @NoArgsConstructor
-    @AllArgsConstructor
     public class ThreadInstance {
+        ThreadInstance(String threadName, ThreadLocalSingleton threadLocalSingleton) {
+            cn.hutool.core.lang.Assert.notNull(threadName);
+            this.threadName = threadName;
+            this.threadLocalSingleton = threadLocalSingleton;
+        }
+
         private String threadName;
         private ThreadLocalSingleton threadLocalSingleton;
     }

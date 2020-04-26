606f716394f366282774a1bf01ea7630b387457c
guozhe <guozhe@renmaitech.com>
2020-04-26 18:53:38 +0800

    增加"spring-boot:配置自动提示"特性实践

---
+++ my-spring-boot/README.md
@@ -0,0 +1,9 @@
+# spring-boot特性实践
+
+## 自动注册监听器
+* MyListener
+
+## 配置自动提示
+* RestTemplateProperties
+* RestTemplateConfig
+* 配置好之后重新build项目就可以提示
---
+++ my-spring-boot/src/test/java/com/nicai/myspringboot/MySpringBootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nicai.myspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = MySpringBootApplication.class)
+@RunWith(SpringRunner.class)
+public class MySpringBootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}

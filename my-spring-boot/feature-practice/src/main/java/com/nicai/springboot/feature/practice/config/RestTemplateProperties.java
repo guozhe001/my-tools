37d949d584348694759fdc44cb51d1b89aabb40a
guozhe <guozhe@renmaitech.com>
2020-04-27 17:53:04 +0800

    spring-boot 配置文件绑定实践加校验

--- my-spring-boot/feature-practice/src/main/java/com/nicai/springboot/feature/practice/config/RestTemplateProperties.java
+++ my-spring-boot/feature-practice/src/main/java/com/nicai/springboot/feature/practice/config/RestTemplateProperties.java
@@ -4,6 +4,10 @@ package com.nicai.springboot.feature.practice.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author guozhe
@@ -11,11 +15,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @Setter
 @Getter
+@Validated
 @ConfigurationProperties("spring.oauth2.rest.template")
 public class RestTemplateProperties {
 
+    @NotBlank
     private String serverUrl;
+    @NotBlank
     private String clientId;
+    @NotNull
     private String secret;
 
 }

f15e43aef0ca51eaece3efd05eadc80a1a7dd122
guozhe <guozhe513@hotmail.com>
2020-06-19 00:55:17 +0800

    分而治之、递归、快速排序

---
+++ my-learn/my-suanfa/src/main/java/com/aha/suanfa/sum/Sum.java
@@ -0,0 +1,15 @@
+package com.aha.suanfa.sum;
+
+/**
+ * @author guozhe
+ */
+public interface Sum {
+
+    /**
+     * 把传入的数组的各个值相加
+     *
+     * @param nums 传入的数组
+     * @return 数组之和
+     */
+    int sum(int[] nums);
+}
---
+++ my-learn/my-suanfa/src/test/java/com/aha/suanfa/sum/SumTest.java
@@ -0,0 +1,76 @@
+package com.aha.suanfa.sum;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
+public class SumTest {
+
+    private static final Sum[] SUMS = new Sum[]{new SumOne(), new SumTwo()};
+
+    @Test
+    public void sum() {
+        for (Sum sumImpl : SUMS) {
+            int sum = sumImpl.sum(new int[]{});
+            Assert.assertEquals(0, sum);
+        }
+    }
+
+    @Test
+    public void sum1() {
+        for (Sum sumImpl : SUMS) {
+            int sum = sumImpl.sum(new int[]{1});
+            Assert.assertEquals(1, sum);
+        }
+    }
+
+    @Test
+    public void sum2() {
+        for (Sum sumImpl : SUMS) {
+            int sum = sumImpl.sum(new int[]{1, 2});
+            Assert.assertEquals(3, sum);
+        }
+    }
+
+    @Test
+    public void sum3() {
+        for (Sum sumImpl : SUMS) {
+            int sum = sumImpl.sum(new int[]{1, 2, 3});
+            Assert.assertEquals(6, sum);
+        }
+
+    }
+
+
+    @Test
+    public void sum4() {
+        for (Sum sumImpl : SUMS) {
+            int sum = sumImpl.sum(IntStream.range(0, 100).toArray());
+            Assert.assertEquals(4950, sum);
+        }
+
+    }
+
+
+    @Test
+    public void sum5() {
+        for (Sum sumImpl : SUMS) {
+            try {
+                sumImpl.sum(IntStream.range(0, 100000).toArray());
+                Assert.assertTrue("期待StackOverflowError没有发生", false);
+            } catch (StackOverflowError e) {
+                Assert.assertTrue(true);
+            }
+        }
+    }
+
+    @Test
+    public void sum6() {
+        for (Sum sumImpl : SUMS) {
+            int sum = sumImpl.sum(new int[]{Integer.MAX_VALUE, 1, 2});
+            Assert.assertTrue(sum < 0);
+        }
+    }
+
+}

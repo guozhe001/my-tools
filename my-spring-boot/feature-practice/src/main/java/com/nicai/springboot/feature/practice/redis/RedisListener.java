399ac9d49391a81a3c6e0d338deedf505158716e
guozhe <guozhe@renmaitech.com>
2020-07-14 13:39:30 +0800

    打印redis监听器的key和value

--- my-spring-boot/feature-practice/src/main/java/com/nicai/springboot/feature/practice/redis/RedisListener.java
+++ my-spring-boot/feature-practice/src/main/java/com/nicai/springboot/feature/practice/redis/RedisListener.java
@@ -3,6 +3,7 @@ package com.nicai.springboot.feature.practice.redis;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 /**
@@ -20,8 +21,18 @@ public class RedisListener implements MessageListener {
      */
     public static final String CHANNEL = "channel:hello";
 
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public RedisListener(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        log.info("RedisListener message.body = {}, message.channel = {}", new String(message.getBody()), new String(message.getChannel()));
+        String key = new String(message.getBody());
+        log.info("RedisListener message.body = {}, message.channel = {}, pattern = {}", key, new String(message.getChannel()),
+                new String(pattern));
+        String value = redisTemplate.opsForValue().get(key);
+        log.info("redis key = {}, value = {}", key, value);
     }
 }
--- my-spring-boot/feature-practice/src/test/java/com/nicai/springboot/feature/practice/controller/RedisControllerTest.java
+++ my-spring-boot/feature-practice/src/test/java/com/nicai/springboot/feature/practice/controller/RedisControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 /**
- *
  * @author guozhe
  * @date 2020/7/13
  */

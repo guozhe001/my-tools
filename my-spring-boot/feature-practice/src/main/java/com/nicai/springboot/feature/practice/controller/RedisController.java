7719be33361a8e38821b5f78155adb82a48eede5
guozhe <guozhe@renmaitech.com>
2020-07-14 14:01:14 +0800

    优化单元测试

--- my-spring-boot/feature-practice/src/main/java/com/nicai/springboot/feature/practice/controller/RedisController.java
+++ my-spring-boot/feature-practice/src/main/java/com/nicai/springboot/feature/practice/controller/RedisController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.stream.IntStream;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author guozhe
@@ -19,6 +19,7 @@ import java.util.stream.IntStream;
 @RequestMapping("/api/redis/")
 public class RedisController {
 
+    private static final int PUBLISH_TIMES = 5;
     private final RedisPublish redisPublish;
 
     public RedisController(RedisPublish redisPublish) {
@@ -27,7 +28,16 @@ public class RedisController {
 
     @PostMapping("publish")
     @ResponseBody
-    public void publish() {
-        IntStream.range(0, 5).forEach(i -> redisPublish.publish(RedisListener.CHANNEL, String.format("message-%s", i)));
+    public void publish() throws InterruptedException {
+        for (int i = 0; i < PUBLISH_TIMES; i++) {
+            try {
+                // 等待监听器接收到
+                TimeUnit.MILLISECONDS.sleep(100L);
+            } catch (InterruptedException e) {
+                log.error("Interrupted error_message = {}", e.getMessage(), e);
+                throw e;
+            }
+            redisPublish.publish(RedisListener.CHANNEL, String.format("message-%s", i));
+        }
     }
 }
--- my-spring-boot/feature-practice/src/test/java/com/nicai/springboot/feature/practice/controller/RedisControllerTest.java
+++ my-spring-boot/feature-practice/src/test/java/com/nicai/springboot/feature/practice/controller/RedisControllerTest.java
@@ -4,20 +4,18 @@ import com.nicai.springboot.feature.practice.BaseWithWebTest;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 /**
  * @author guozhe
  * @date 2020/7/13
  */
-@AutoConfigureMockMvc
 public class RedisControllerTest extends BaseWithWebTest {
 
     @Autowired
     private RedisController redisController;
 
     @Test
-    public void publish() {
+    public void publish() throws InterruptedException {
         redisController.publish();
         Assert.assertTrue(true);
     }

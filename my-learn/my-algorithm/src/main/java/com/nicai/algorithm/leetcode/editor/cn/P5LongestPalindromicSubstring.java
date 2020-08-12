7ff7b7f89f7dee397625aeb0abfd03e84a184909
guozhe <guozhe@renmaitech.com>
2020-08-12 16:52:42 +0800

    添加算法-"最长回文子串"

--- my-learn/my-algorithm/src/main/java/com/nicai/algorithm/leetcode/editor/cn/P5LongestPalindromicSubstring.java
+++ my-learn/my-algorithm/src/main/java/com/nicai/algorithm/leetcode/editor/cn/P5LongestPalindromicSubstring.java
@@ -16,13 +16,20 @@ package com.nicai.algorithm.leetcode.editor.cn;
 // Related Topics 字符串 动态规划 
 // 👍 2391 👎 0
 
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
+/**
+ * @author nicai
+ */
 public class P5LongestPalindromicSubstring {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public String longestPalindrome(String s) {
-            if (Objects.isNull(s)) {
+            System.out.println("s=" + s);
+            if (Objects.isNull(s) || s.length() <= 1) {
                 return s;
             }
             /*
@@ -40,30 +47,53 @@ public class P5LongestPalindromicSubstring {
             return solution1(s);
         }
 
-
-    }
-
-    /**
-     * #方法1，循环字符串数组，依此取字符，然后在剩余的字符中查找此字符首次出现的下标
-     * ## 如果能找到：此时回文子串的长度为下标+2
-     * ### 并用map1保存长度，key为字符，value为长度
-     * ### 用map2保存位置，key为长度，value为回文子串
-     * ### 如果当前的最大长度大于历史的最大长度，更新最大长度
-     * ### 如果当前的最大长度大于等于总长度减当前下标减1，则结束循环
-     * ### 最终从map2中get当前最大长度的值，返回
-     * 如果找不到，继续下次循环
-     *
-     * @param s 字符串
-     * @return 最长回文字串
-     */
-    private String solution1(String s) {
-        char[] chars = s.toCharArray();
-//        Map<String,>
-        for (int i = 0; i < chars.length - 1; i++) {
-
+        /**
+         * #方法1，循环字符串数组，依次取字符，然后在剩余的字符中从后往前查找此字符首次出现的下标
+         * ## 如果能找到：此时回文子串的长度为两个下标的差 + 1
+         * ### 记录最大的长度
+         * ### 用map保存位置，key为长度，value为回文子串
+         * ### 如果当前的最大长度大于历史的最大长度，更新最大长度
+         * ### 如果当前的最大长度大于等于总长度减当前下标减1，则结束循环
+         * ### 最终从map中get当前最大长度的值，返回
+         * 如果找不到，继续下次循环
+         *
+         * @param s 字符串
+         * @return 最长回文字串
+         */
+        private String solution1(String s) {
+            char[] chars = s.toCharArray();
+            int maxLength = 0;
+            Map<Integer, String> lengthValueMap = new HashMap<>();
+            for (int i = 0; i < chars.length - 1; i++) {
+                // 当前的字符
+                char currentChar = chars[i];
+                // 定义当前字符的回文字符串的最大长度为1
+                int length = 1;
+                // 从当前位置的下一位出发，遍历整个字符数组，寻找第一个与当前的字符相同的字符
+                for (int j = chars.length - 1; j > i; j--) {
+                    char nextChar = chars[j];
+                    // 如果找到了相同的字符，则计算两个字符的长度，并记录下标
+                    if (nextChar == currentChar) {
+                        // 当前回文字符串长度
+                        length = j - i + 1;
+                    }
+                    if (length > maxLength) {
+                        System.out.println(String.format("i=%s, j=%s, currentChar=%s, length=%s", i, j, currentChar, length));
+                        maxLength = length;
+                        lengthValueMap.put(length, String.valueOf(chars, i, length));
+                        if (maxLength > 1) {
+                            break;
+                        }
+                    }
+                }
+                if (maxLength >= chars.length - i - 1) {
+                    break;
+                }
+            }
+            return lengthValueMap.get(maxLength);
         }
-        return null;
     }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
--- my-learn/my-algorithm/src/test/java/com/nicai/algorithm/leetcode/editor/cn/P5LongestPalindromicSubstringTest.java
+++ my-learn/my-algorithm/src/test/java/com/nicai/algorithm/leetcode/editor/cn/P5LongestPalindromicSubstringTest.java
@@ -1,27 +1,94 @@
-//package com.leetcode.editor.cn;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-///**
-// * @author guozhe
-// * @date 2020/7/6
-// */
-//public class P5LongestPalindromicSubstringTest {
-//
-//    P5LongestPalindromicSubstring.Solution solution = new P5LongestPalindromicSubstring().new Solution();
-//
-//
-//    /**
-//     * 示例 1：
-//     * 输入: "babad"
-//     * 输出: "bab"
-//     * 注意: "aba" 也是一个有效答案。
-//     */
-//    @Test
-//    public void testCase1() {
-//        String result = solution.longestPalindrome("babad");
-//        Assert.assertEquals("bab", result);
-//    }
-//
-//}
+package com.nicai.algorithm.leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author guozhe
+ * @date 2020/7/6
+ */
+public class P5LongestPalindromicSubstringTest {
+
+    P5LongestPalindromicSubstring.Solution solution = new P5LongestPalindromicSubstring().new Solution();
+
+    /**
+     * 示例 1：
+     * 输入: "babad"
+     * 输出: "bab"
+     * 注意: "aba" 也是一个有效答案。
+     */
+    @Test
+    public void testCase1() {
+        String result = solution.longestPalindrome("babad");
+        Assert.assertEquals("bab", result);
+    }
+
+    /**
+     * 示例 2：
+     * <p>
+     * 输入: "cbbd"
+     * 输出: "bb"
+     */
+    @Test
+    public void testCase2() {
+        String result = solution.longestPalindrome("cbbd");
+        Assert.assertEquals("bb", result);
+    }
+
+    /**
+     * 输入 null
+     */
+    @Test
+    public void testCaseNull() {
+        Assert.assertNull(solution.longestPalindrome(null));
+    }
+
+    /**
+     * 输入 ""
+     */
+    @Test
+    public void testCaseBlank() {
+        Assert.assertEquals("", solution.longestPalindrome(""));
+    }
+
+    /**
+     * 输入 a
+     */
+    @Test
+    public void testCaseOneChar() {
+        Assert.assertEquals("a", solution.longestPalindrome("a"));
+    }
+
+    /**
+     * 输入 ac
+     */
+    @Test
+    public void testCaseTwoChar() {
+        Assert.assertEquals("a", solution.longestPalindrome("ac"));
+    }
+
+    /**
+     * 输入 aa
+     */
+    @Test
+    public void testCaseTwoSameChar() {
+        Assert.assertEquals("aa", solution.longestPalindrome("aa"));
+    }
+
+    /**
+     * 输入 aaa
+     */
+    @Test
+    public void testCaseThreeSameChar() {
+        Assert.assertEquals("aaa", solution.longestPalindrome("aaa"));
+    }
+
+
+    @Test
+    public void testStringValueOf() {
+        char[] chars = new char[]{'a', 'b', 'c', 'd', 'e', 'f'};
+        String s = String.valueOf(chars, 0, 3);
+        Assert.assertEquals("abc", s);
+
+    }
+}

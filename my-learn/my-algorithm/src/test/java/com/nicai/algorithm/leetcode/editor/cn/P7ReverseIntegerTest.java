9a8dea8499389d6c29a9f25571e79aa0b072ea80
guozhe <guozhe@renmaitech.com>
2020-09-21 14:42:17 +0800

    添加整数反转

---
+++ my-learn/my-algorithm/src/test/java/com/nicai/algorithm/leetcode/editor/cn/P7ReverseIntegerTest.java
@@ -0,0 +1,49 @@
+package com.nicai.algorithm.leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * P7ReverseIntegerTest
+ *
+ * @author guozhe
+ * @date 2020/09/21
+ */
+public class P7ReverseIntegerTest {
+    P7ReverseInteger.Solution solution = new P7ReverseInteger().new Solution();
+
+    /**
+     * 示例 1:
+     * 输入: 123
+     * 输出: 321
+     */
+    @Test
+    public void reverse() {
+        int reverse = solution.reverse(123);
+        Assert.assertEquals(321, reverse);
+    }
+
+    /**
+     * 示例 2:
+     * <p>
+     * 输入: -123
+     * 输出: -321
+     */
+    @Test
+    public void reverse2() {
+        int reverse = solution.reverse(-123);
+        Assert.assertEquals(-321, reverse);
+    }
+
+    /**
+     * 示例 3:
+     * <p>
+     * 输入: 120
+     * 输出: 21
+     */
+    @Test
+    public void reverse3() {
+        int reverse = solution.reverse(120);
+        Assert.assertEquals(21, reverse);
+    }
+}

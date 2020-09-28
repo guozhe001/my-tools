4e4a407c5f4af32465c8868759a108356324ef1f
guozhe <guozhe@renmaitech.com>
2020-09-28 18:15:00 +0800

    leetcode：完成字符串相乘

--- my-learn/my-algorithm/src/main/java/com/nicai/algorithm/leetcode/editor/cn/P43MultiplyStrings.java
+++ my-learn/my-algorithm/src/main/java/com/nicai/algorithm/leetcode/editor/cn/P43MultiplyStrings.java
@@ -22,6 +22,10 @@ package com.nicai.algorithm.leetcode.editor.cn;
 // Related Topics 数学 字符串 
 // 👍 489 👎 0
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * 字符串相乘
  *
@@ -31,21 +35,130 @@ public class P43MultiplyStrings {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public String multiply(String num1, String num2) {
-            /**
-             * 转换成数组，然后让
-             */
-            int[] ints1 = getIntArray(num1);
-            int[] ints2 = getIntArray(num2);
-            return null;
+            // 转换成数组，然后让数组相乘
+            List<String> multiply = multiply(getIntArray(num1), getIntArray(num2));
+            return addAllString(multiply);
+        }
+
+        /**
+         * 把数值相加
+         *
+         * @param strings 包含数字的字符串列表
+         * @return 所有的字符串之和
+         */
+        private String addAllString(List<String> strings) {
+            if (strings.size() == 1) {
+                return strings.get(0);
+            } else {
+                List<String> first = new ArrayList<>();
+                first.add(strings.get(0));
+                strings.remove(0);
+                return add(addAllString(first), addAllString(strings));
+            }
+        }
+
+        /**
+         * 两个字符串相加
+         *
+         * @param str1 字符串1
+         * @param str2 字符串2
+         * @return 相加后的字符串
+         */
+        private String add(String str1, String str2) {
+            return add(stringToStack(str1), stringToStack(str2));
+        }
+
+        /**
+         * 两个数组相加
+         *
+         * @param stack1 栈1
+         * @param stack2 栈2
+         * @return 相加后的字符串
+         */
+        private String add(Stack<Integer> stack1, Stack<Integer> stack2) {
+            int max = Math.max(stack1.size(), stack2.size());
+            // 保存进位的栈
+            Stack<Integer> carryStack = new Stack<>();
+            Stack<Integer> resultStack = new Stack<>();
+            for (int i = 0; i < max; i++) {
+                int n1 = stack1.isEmpty() ? 0 : stack1.pop();
+                int n2 = stack2.isEmpty() ? 0 : stack2.pop();
+                int carry = carryStack.isEmpty() ? 0 : carryStack.pop();
+                int result = n1 + n2 + carry;
+                carryStack.push(result / 10);
+                resultStack.push(result % 10);
+            }
+            resultStack.push(carryStack.pop());
+            return stackToString(resultStack);
+        }
+
+        /**
+         * 数组相乘
+         *
+         * @param ints1 数组1
+         * @param ints2 数组2
+         * @return 数组相乘之后的结果
+         */
+        private List<String> multiply(int[] ints1, int[] ints2) {
+            // 保存进位的栈
+            Stack<Integer> carryStack = new Stack<>();
+            List<String> numStrings = new ArrayList<>();
+            for (int i = ints1.length - 1; i >= 0; i--) {
+                int int1 = ints1[i];
+                Stack<Integer> resultStack = new Stack<>();
+                for (int k = 0; k < ints1.length - 1 - i; k++) {
+                    resultStack.push(0);
+                }
+                for (int j = ints2.length - 1; j >= 0; j--) {
+                    int int2 = ints2[j];
+                    int result = int1 * int2;
+                    int carry = carryStack.isEmpty() ? 0 : carryStack.pop();
+                    result = result + carry;
+                    int remainder = result % 10;
+                    if (j == 0) {
+                        resultStack.push(remainder);
+                        if (result / 10 > 0) {
+                            resultStack.push(result / 10);
+                        }
+                    } else {
+                        resultStack.push(remainder);
+                        carryStack.push(result / 10);
+                    }
+                }
+                numStrings.add(stackToString(resultStack));
+            }
+            return numStrings;
+        }
+
+        private String stackToString(Stack<Integer> stack) {
+            StringBuilder sb = new StringBuilder();
+            int size = stack.size();
+            for (int i = 0; i < size; i++) {
+                Integer pop = stack.pop();
+                if (pop == 0 && sb.length() == 0 && i < size - 1) {
+                    continue;
+                }
+                sb.append(pop);
+            }
+            return sb.toString();
         }
 
         private int[] getIntArray(String num) {
             char[] chars = num.toCharArray();
             int[] result = new int[chars.length];
             for (int i = 0; i < chars.length; i++) {
-                result[i] = chars[i];
+                result[i] = Integer.parseInt(String.valueOf(chars[i]));
             }
             return result;
+        }
+
+        private Stack<Integer> stringToStack(String num) {
+            char[] chars = num.toCharArray();
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < chars.length; i++) {
+                stack.push(Integer.parseInt(String.valueOf(chars[i])));
+            }
+            return stack;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
--- my-learn/my-algorithm/src/test/java/com/nicai/algorithm/leetcode/editor/cn/P43MultiplyStringsTest.java
+++ my-learn/my-algorithm/src/test/java/com/nicai/algorithm/leetcode/editor/cn/P43MultiplyStringsTest.java
@@ -1,8 +1,13 @@
 package com.nicai.algorithm.leetcode.editor.cn;
 
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
+import org.powermock.reflect.Whitebox;
+
+import java.util.List;
+import java.util.Stack;
 
 /**
  * P43MultiplyStringsTest
@@ -40,17 +45,78 @@ public class P43MultiplyStringsTest {
     /**
      * 示例 3:
      * <p>
+     * 输入: num1 = "99999", num2 = "0"
+     * 输出: "0"
+     */
+    @Test
+    public void multiply3() {
+        invokeAndAssert("99999", "0", "0");
+    }
+
+    /**
+     * 示例 4:
+     * <p>
      * 输入: num1 = "2147483647", num2 = "2"
      * 输出: "4294967294"
      */
     @Test
-    public void multiply3() {
+    public void multiply4() {
         log.info("Integer.MAX_VALUE={}, Integer.MAX_VALUE * 2 = {}", Integer.MAX_VALUE, Integer.MAX_VALUE * 2L);
         invokeAndAssert("2147483647", "2", "4294967294");
     }
 
+    /**
+     * 示例 5:
+     * <p>
+     * 输入: num1 = "123456789", num2 = "2147483647"
+     * 输出: "265121435488629483"
+     */
+    @Test
+    public void multiply5() {
+        log.info("Integer.MAX_VALUE={}, Integer.MAX_VALUE * 2 = {}", Integer.MAX_VALUE, Integer.MAX_VALUE * 2L);
+        invokeAndAssert("123456789", "2147483647", "265121435488629483");
+    }
+
     private void invokeAndAssert(String num1, String num2, String expect) {
         Assert.assertEquals(expect, solution.multiply(num1, num2));
+    }
+
+    @Test
+    public void getIntArray() throws Exception {
+        int[] result = Whitebox.invokeMethod(solution, "getIntArray", "12328374");
+        int[] expect = new int[]{1, 2, 3, 2, 8, 3, 7, 4};
+        for (int i = 0; i < expect.length; i++) {
+            Assert.assertEquals(expect[i], result[i]);
+        }
+    }
+
+    @Test
+    public void multiplyIntArray() throws Exception {
+        List<String> strings = Whitebox.invokeMethod(solution, "multiply", new int[]{1, 2, 8}, new int[]{2, 4, 6});
+        Assert.assertEquals(3, strings.size());
+        Assert.assertTrue((strings.contains("768") && strings.contains("5120") && strings.contains("25600"))
+                || (strings.contains("1968") && strings.contains("4920") && strings.contains("24600")));
+    }
+
+    @Test
+    public void addIntArray() throws Exception {
+        String str = Whitebox.invokeMethod(solution, "add", new int[]{1, 2, 8}, new int[]{2, 4, 6});
+        Assert.assertEquals("374", str);
+    }
+
+    @Test
+    public void addAllString() throws Exception {
+        String str = Whitebox.invokeMethod(solution, "addAllString", Lists.newArrayList("123", "321", "1000"));
+        Assert.assertEquals("1444", str);
+    }
+
+    @Test
+    public void stringToStack() throws Exception {
+        Stack<Integer> stack = Whitebox.invokeMethod(solution, "stringToStack", "12323278");
+        int[] ints = new int[]{8, 7, 2, 3, 2, 3, 2, 1};
+        for (int i = 0; i < ints.length; i++) {
+            Assert.assertEquals(Integer.valueOf(ints[i]), stack.pop());
+        }
     }
 
 }

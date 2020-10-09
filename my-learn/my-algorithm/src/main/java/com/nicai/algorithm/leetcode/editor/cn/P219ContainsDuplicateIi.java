83df8724b54dc6917676f6c45f85a7917e8a6e73
guozhe <guozhe@renmaitech.com>
2020-10-09 11:56:59 +0800

    leetcode：init 存在重复元素 II

---
+++ my-learn/my-algorithm/src/main/java/com/nicai/algorithm/leetcode/editor/cn/P219ContainsDuplicateIi.java
@@ -0,0 +1,38 @@
+package com.nicai.algorithm.leetcode.editor.cn;
+//给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值
+// 至多为 k。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: nums = [1,2,3,1], k = 3
+//输出: true 
+//
+// 示例 2: 
+//
+// 输入: nums = [1,0,1,1], k = 1
+//输出: true 
+//
+// 示例 3: 
+//
+// 输入: nums = [1,2,3,1,2,3], k = 2
+//输出: false 
+// Related Topics 数组 哈希表 
+// 👍 204 👎 0
+
+/**
+ * 存在重复元素 II
+ *
+ * @author nicai
+ */
+public class P219ContainsDuplicateIi {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ my-learn/my-algorithm/src/test/java/com/nicai/algorithm/leetcode/editor/cn/P219ContainsDuplicateIiTest.java
@@ -0,0 +1,49 @@
+package com.nicai.algorithm.leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * P219ContainsDuplicateIiTest
+ *
+ * @author guozhe
+ * @date 2020/09/30
+ */
+public class P219ContainsDuplicateIiTest {
+
+    private P219ContainsDuplicateIi.Solution solution = new P219ContainsDuplicateIi().new Solution();
+
+    /**
+     * 示例 1:
+     * <p>
+     * 输入: nums = [1,2,3,1], k = 3
+     * 输出: true
+     */
+    @Test
+    public void containsNearbyDuplicate() {
+        Assert.assertTrue(solution.containsNearbyDuplicate(new int[]{1, 2, 3, 1}, 3));
+    }
+
+    /**
+     * 示例 2:
+     * <p>
+     * 输入: nums = [1,0,1,1], k = 1
+     * 输出: true
+     */
+    @Test
+    public void containsNearbyDuplicate1() {
+        Assert.assertTrue(solution.containsNearbyDuplicate(new int[]{1, 0, 1, 1}, 1));
+    }
+
+    /**
+     * 示例 3:
+     * <p>
+     * 输入: nums = [1,2,3,1,2,3], k = 2
+     * 输出: false
+     */
+    @Test
+    public void containsNearbyDuplicate2() {
+        Assert.assertTrue(solution.containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2));
+    }
+
+}

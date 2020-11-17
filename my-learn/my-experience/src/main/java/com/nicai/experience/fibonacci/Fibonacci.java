5886985025ef400dbba08aa89da1bd331f42790c
guozhe <guozhe513@hotmail.com>
2020-11-17 23:01:00 +0800

    优化斐波那契数列

--- my-learn/my-experience/src/main/java/com/nicai/experience/fibonacci/Fibonacci.java
+++ my-learn/my-experience/src/main/java/com/nicai/experience/fibonacci/Fibonacci.java
@@ -1,12 +1,14 @@
 package com.nicai.experience.fibonacci;
 
 import com.nicai.util.Generator;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 斐波那契数列生成器
  *
  * @author guozhe
  */
+@Slf4j
 public class Fibonacci implements Generator<Integer> {
 
     private int count = 0;
@@ -16,18 +18,33 @@ public class Fibonacci implements Generator<Integer> {
         return fib(count++);
     }
 
-    private Integer fib(int n) {
-        if (n < 2) {
-            return n;
+//    private Integer fib(int n) {
+//        if (n < 2) {
+//            return n;
+//        }
+//        return fib(n - 2) + fib(n - 1);
+//    }
+
+    public int fib(int n) {
+        assert n >= 0;
+        int[] c = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            if (i < 2) {
+                c[i] = i;
+            } else {
+                c[i] = c[i - 2] + c[i - 1];
+            }
         }
-        return fib(n - 2) + fib(n - 1);
+        return c[n];
     }
 
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci();
+        long l = System.currentTimeMillis();
         for (int i = 0; i < 46; i++) {
             System.out.print(fibonacci.next() + " ");
         }
+        System.out.println("");
+        log.info("spend {} milliseconds", System.currentTimeMillis() - l);
     }
-
 }

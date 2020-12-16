8c9974e2efbe4b546c92d9d42e384e7fa48e661f
guozhe <guozhe@renmaitech.com>
2020-12-16 19:00:22 +0800

    leetcode：290,给定一种规律 pattern 和一个字符串 str ，判断 str 是否遵循相同的规律。

---
+++ my-learn/my-algorithm/src/main/java/com/nicai/algorithm/leetcode/editor/cn/P290WordPattern.java
@@ -0,0 +1,63 @@
+package com.nicai.algorithm.leetcode.editor.cn;
+//给定一种规律 pattern 和一个字符串 str ，判断 str 是否遵循相同的规律。 
+//
+// 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 str 中的每个非空单词之间存在着双向连接的对应规律。 
+//
+// 示例1: 
+//
+// 输入: pattern = "abba", str = "dog cat cat dog"
+//输出: true 
+//
+// 示例 2: 
+//
+// 输入:pattern = "abba", str = "dog cat cat fish"
+//输出: false 
+//
+// 示例 3: 
+//
+// 输入: pattern = "aaaa", str = "dog cat cat dog"
+//输出: false 
+//
+// 示例 4: 
+//
+// 输入: pattern = "abba", str = "dog dog dog dog"
+//输出: false 
+//
+// 说明: 
+//你可以假设 pattern 只包含小写字母， str 包含了由单个空格分隔的小写字母。 
+// Related Topics 哈希表 
+// 👍 267 👎 0
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P290WordPattern {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public boolean wordPattern(String pattern, String s) {
+            String[] words = s.split(" ");
+            char[] patternChars = pattern.toCharArray();
+            if (patternChars.length != words.length) {
+                return false;
+            }
+            //key为pattern的字母，value为该字母对应的单词
+            Map<String, String> patternMap = new HashMap<>(pattern.length());
+            for (int i = 0; i < patternChars.length; i++) {
+                String key = String.valueOf(patternChars[i]);
+                // 如果map中已经存在此key，则验证map中的value和当前的值是否相同
+                if (patternMap.containsKey(key)) {
+                    // 如果不相同，直接返回false
+                    if (!patternMap.get(key).equals(words[i])) {
+                        return false;
+                    }
+                } else {
+                    patternMap.put(key, words[i]);
+                }
+            }
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ my-learn/my-algorithm/src/test/java/com/nicai/algorithm/leetcode/editor/cn/P290WordPatternTest.java
@@ -0,0 +1,59 @@
+package com.nicai.algorithm.leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class P290WordPatternTest {
+
+    private final P290WordPattern.Solution solution = new P290WordPattern().new Solution();
+
+    /**
+     * 示例1:
+     * <p>
+     * 输入: pattern = "abba", str = "dog cat cat dog"
+     * 输出: true
+     */
+    @Test
+    public void wordPattern() {
+        invoke("abba", "dog cat cat dog", true);
+    }
+
+    /**
+     * 示例 2:
+     * <p>
+     * 输入:pattern = "abba", str = "dog cat cat fish"
+     * 输出: false
+     */
+    @Test
+    public void wordPattern2() {
+        invoke("abba", "dog cat cat fish", false);
+    }
+
+    /**
+     * 示例 3:
+     * <p>
+     * 输入: pattern = "aaaa", str = "dog cat cat dog"
+     * 输出: false
+     */
+    @Test
+    public void wordPattern3() {
+        invoke("aaaa", "dog cat cat dog", false);
+    }
+
+    /**
+     * 示例 4:
+     * <p>
+     * 输入: pattern = "abba", str = "dog dog dog dog"
+     * 输出: false
+     */
+    @Test
+    public void wordPattern4() {
+        invoke("abba", "dog dog dog dog", false);
+    }
+
+
+    private void invoke(String pattern, String str, boolean expect) {
+        Assert.assertEquals(expect, solution.wordPattern(pattern, str));
+    }
+
+}

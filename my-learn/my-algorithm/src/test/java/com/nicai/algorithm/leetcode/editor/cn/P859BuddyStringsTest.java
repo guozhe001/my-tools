921a2dac639db4baa52bc9b74dc0d91a3ccd18f5
guozhe <guozhe@renmaitech.com>
2021-01-07 18:53:38 +0800

    leetcode：暴力解决亲密字符串

---
+++ my-learn/my-algorithm/src/test/java/com/nicai/algorithm/leetcode/editor/cn/P859BuddyStringsTest.java
@@ -0,0 +1,73 @@
+package com.nicai.algorithm.leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class P859BuddyStringsTest {
+    private final P859BuddyStrings.Solution solution = new P859BuddyStrings().new Solution();
+
+
+    /**
+     * 示例 1：
+     * <p>
+     * <p>
+     * 输入： A = "ab", B = "ba"
+     * 输出： true
+     * 解释： 你可以交换 A[0] = 'a' 和 A[1] = 'b' 生成 "ba"，此时 A 和 B 相等。
+     */
+    @Test
+    public void buddyStrings() {
+        Assert.assertTrue(solution.buddyStrings("ab", "ba"));
+    }
+
+
+    /**
+     * 示例 2：
+     * <p>
+     * <p>
+     * 输入： A = "ab", B = "ab"
+     * 输出： false
+     * 解释： 你只能交换 A[0] = 'a' 和 A[1] = 'b' 生成 "ba"，此时 A 和 B 不相等。
+     */
+    @Test
+    public void buddyStrings2() {
+        Assert.assertFalse(solution.buddyStrings("ab", "ab"));
+    }
+
+    /**
+     * 示例 3:
+     * <p>
+     * <p>
+     * 输入： A = "aa", B = "aa"
+     * 输出： true
+     * 解释： 你可以交换 A[0] = 'a' 和 A[1] = 'a' 生成 "aa"，此时 A 和 B 相等。
+     */
+    @Test
+    public void buddyStrings3() {
+        Assert.assertTrue(solution.buddyStrings("aa", "aa"));
+    }
+
+    /**
+     * 示例 4：
+     * <p>
+     * <p>
+     * 输入： A = "aaaaaaabc", B = "aaaaaaacb"
+     * 输出： true
+     */
+    @Test
+    public void buddyStrings4() {
+        Assert.assertTrue(solution.buddyStrings("aaaaaaabc", "aaaaaaacb"));
+    }
+
+    /**
+     * 示例 5：
+     * <p>
+     * <p>
+     * 输入： A = "", B = "aa"
+     * 输出： false
+     */
+    @Test
+    public void buddyStrings5() {
+        Assert.assertFalse(solution.buddyStrings("", "aa"));
+    }
+}
